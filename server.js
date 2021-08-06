128d6e71577a88042d642da9f2782e1300ea1ee5
Pavan Srinivas <39332902+nivaskambhampati1998@users.noreply.github.com>
2021-08-06 17:33:30 +0530

    server.js

--- server.js
+++ server.js
@@ -1,24 +1,44 @@
 const express = require('express');
-const mongoose  = require('mongoose');
-const app = require('./app');
+const app = express();
+const dotEnv = require('dotenv');
+const mongoose = require('mongoose');
+const cors = require('cors');
 
-const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+// configure cors
+app.use(cors());
 
-const DB = process.env.DATABASE;
-mongoose
-  .connect(DB, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("database connected successfully");
-  });
+// configure express to receive the form data
+app.use(express.json());
 
+// configure dotEnv
+dotEnv.config({path : './.env'});
 
+const hostname = process.env.LOCAL_HOST_NAME;
+const port = process.env.LOCAL_PORT;
 
-port = process.env.PORT || 8000;
-app.listen(port,()=>{
-  console.log(`server is running on ${port}`)
+// connect to Mongo DB
+mongoose.connect(process.env.MONGO_DB_URL, {
+    useFindAndModify : false,
+    useNewUrlParser : true,
+    useUnifiedTopology : true,
+    useCreateIndex : true
+}).then((response) => {
+    console.log(`Connected to MongoDB successfully`);
+}).catch((error) => {
+    console.error(error);
+    process.exit(1);
 });
+
+// basic request
+app.get('/', (request , response) => {
+    response.send(`Welcome to User Authentication`);
+});
+
+// router configuration
+app.use('/api/users' , require('./router/userRouter'));
+
+app.listen(port, hostname, () => {
+    console.log(`Express Server is started at http://${hostname}:${port}`);
+});
+
+
